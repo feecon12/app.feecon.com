0a3cb957dd64d115c03cb73f8cca507e3fda182d
Feecon <mail2feeconbehera@gmail.com>
2023-08-06 20:53:13 +0530

    new

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -1,26 +1,28 @@
 import React from 'react'
-import Link from 'next/link'
+import { Logo } from './Logo'
 
-const CustomLink = ({ href, title, className = "" }) => {
-
-  return (
-    <Link href={href} >
-      {title}
-    </Link>
-  )
-}
 
 export const NavBar = () => {
   return (
-    <header className='w-full px-32 py-8 font-medium flex items-center justify-between'>
+    <header className='px-32 py-8 items-center flex justify-between font-medium'>
       <nav>
-        <CustomLink href={"/home"} title="Home" className='mr-4'/>
-        <CustomLink href={"/about"} title="About" />
-        <CustomLink href={"/articles"} title="Articles"  />
-        <CustomLink href={"/project"} title="Project"  />
-      
+        <a href={'/'} className='mr4'> Home</a>
+        <a href={'/'} className='mx-4'> About</a>
+        <a href={'/'} className='mx-4'> Articles</a>
+        <a href={'/'} className='ml-4'> Projects</a>
+        
       </nav>
-    
+
+      <Logo/>
+
+      <nav>
+        <a href='/'className='mr-4'>Icon1</a>
+        <a href='/'className='mx-4'>Icon2</a>
+        <a href='/'className='mx-4'>Icon3</a>
+        <a href='/'className='ml-4'>Icon4</a>
+      </nav>
+
+
     </header>
   )
 }
