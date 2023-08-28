bf99d917283c69c7aeb45297cd66b8565862fb09
Feecon <mail2feeconbehera@gmail.com>
2023-08-28 12:20:02 +0530

    Resume changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
 
               </p>
               <div className='flex items-center self-start mt-2 lg:self-center'>
-                <Link href='/FeeconBehera.pdf' target={'_blank'} className='flex items-center bg-dark text-light p-2 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid hover:border-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:p-2 md:px-4 md:text-base '
+                <Link href='/FeeconBeheraResume.pdf' target={'_blank'} className='flex items-center bg-dark text-light p-2 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid hover:border-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:p-2 md:px-4 md:text-base '
                   download={true}
                 >Resume <LinkArrow className={'w-5 ml-1'} />
                 </Link>
